31583d77e7944180ae08439bb667f05e4d04da94
Samuel Miller <kamn.miller@gmail.com>
2016-12-15 20:50:39 -0700

    Updated gitignore and project clj

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :uberjar-name "MyBot.jar"
-  :main Clojite_v1
+  :main Clojite_v3
   :profiles {:uberjar {:aot :all}})
   
