45d8199bf0536309d595733fe8039444d046b04b
Samuel Miller <kamn.miller@gmail.com>
2016-12-15 22:15:41 -0700

    Added the ability to get to the closest site

--- src/Clojite_v3.clj
+++ src/Clojite_v3.clj
@@ -4,6 +4,9 @@
   (:gen-class))
 
 
+;; Info before v3.1
+;; Rank 536
+;; Points 28.2
 
 ;; Changes
 ;; ===
@@ -15,6 +18,10 @@
 ;;    - Added error cathcing when getting a site. Not sure why the error happens
 ;;      I think that maybe the parsing is wrong for the game-map
 
+;; 3.1 (To get in sync with halite versioning of my bot)
+;; 1. Increased the wait time from a product to generate more power
+;;    - Also make the site move if above 250
+;; 2. Closest direction implemented
 
 (def bot-name "Clojite v3")
 
@@ -42,6 +49,60 @@
   (get-at game-map (dec (:x site)) (:y site)))
 
 
+(defn distance-x [map-size a b]
+  "Calculate the distance between two sites along the x axis"
+  (let [dist-left (mod (- (:x a) (:x b)) map-size)
+        dist-right (mod  (+ (- map-size (:x a)) (:x b)) map-size)]
+    (min dist-left dist-right)))
+
+(defn direction-x [map-size a b]
+    (let [dist-left (mod (- (:x a) (:x b)) map-size)
+          dist-right (mod  (+ (- map-size (:x a)) (:x b)) map-size)]
+      (if (< dist-left dist-right)
+          :west
+          :east)))
+  
+
+(defn distance-y [map-size a b]
+  "Calculate the distance between two sites along the y axis"
+  (let [dist-up (mod (- (:y a) (:y b)) map-size)
+        dist-down (mod  (+ (- map-size (:y a)) (:y b)) map-size)]
+    (min dist-down dist-up)))
+
+(defn direction-y [map-size a b]
+    (let [dist-north (mod (- (:y a) (:y b)) map-size)
+          dist-south (mod  (+ (- map-size (:y a)) (:y b)) map-size)]
+      (if (< dist-north dist-south)
+          :north
+          :south)))
+
+(defn distance 
+  "Calculate the distance between two sites"
+  [map-size a b]
+  (let [dist-x (distance-x map-size a b)
+        dist-y (distance-y map-size a b)]
+   (+ dist-x dist-y)))
+
+(defn get-closest-site [my-id game-map site]
+  (->> game-map
+       flatten
+       (filter #(not= (:owner %) my-id))
+       (reduce (fn [r x] 
+                (if (< (distance (count game-map) site r) (distance (count game-map) site x)) r x)))))
+
+(defn get-closest-site-direction [my-id game-map site]
+  (let [closest-site (get-closest-site my-id game-map site)
+        dist-x (distance-x (count game-map) site closest-site)
+        dist-y (distance-y (count game-map) site closest-site)
+        dir-x (direction-x (count game-map) site closest-site)
+        dir-y (direction-y (count game-map) site closest-site)]
+    (cond
+      (zero? dist-y) [site dir-x]
+      (zero? dist-x) [site dir-y]
+      (< dist-x dist-y) [site dir-x]
+      :else [site dir-y])))
+
+
 (defn get-lowest-strength-site [my-id sites]
   "Given a list of sites finds the one with the lowest strength"
   (let [filtered (->> sites
@@ -53,7 +114,7 @@
 (defn get-lowest-direction
   "Given a cell that I own figure out the closest cell next to me that is not owned by me. Send that way."
   [my-id game-map [site orig-direction]]
-  (if (< (:strength site) (* 4 (:production site)))
+  (if (and (< (:strength site) (* 6 (:production site))) (<= (:strength site) 250))
     [site :still]
     ;;TODO: Adjecent sites fn
     (let [north-site (get-north-site game-map site)
@@ -62,7 +123,7 @@
           west-site (get-west-site game-map site)
           weak-site (get-lowest-strength-site my-id [north-site east-site south-site west-site])]
       (if (nil? weak-site)
-        [site (rand-nth [:west :north])]
+        (get-closest-site-direction my-id game-map site)
         (if (<= (:strength site) (:strength weak-site))
          [site :still]
          (cond 
